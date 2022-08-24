461d02847c09e6df120ed19c15c81c7dab7615a0
Oumar FALL <oumarfall@hotmail.fr>
2022-08-24 17:37:33 +0000

    refactor du bloc des évènements similaires

--- web/modules/agenda/src/Plugin/Block/SimilarEventsBlock.php
+++ web/modules/agenda/src/Plugin/Block/SimilarEventsBlock.php
@@ -16,40 +16,47 @@ use Drupal\Core\Block\BlockBase;
  *   category = @Translation("Adimeo")
  * )
  */
-class SimilarEventsBlock extends BlockBase {
+class SimilarEventsBlock extends BlockBase
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function build()
+    {
+        $current_nid   = \Drupal::routeMatch()->getParameter('node')->Id();
+        $current_event = Node::load($current_nid);
+        $event_type_id = $current_event->field_event_type->target_id;
 
-  /**
-   * {@inheritdoc}
-   */
-  public function build() {
-    $current_nid   = \Drupal::routeMatch()->getParameter('node')->Id();
-    $current_event = Node::load($current_nid);
+        $query = \Drupal::entityQuery('node')
+            ->condition('type', 'event')
+            ->condition('field_event_type', $event_type_id)
+            ->condition('status', NodeInterface::PUBLISHED)
+            ->condition('nid', $current_event->id(), '<>')
+            ->sort('nid')
+            ->range(0, 3);
+        $nids = $query->execute();
 
-    $query = \Drupal::entityQuery('node')
-              ->condition('type', 'event')
-              ->condition('field_event_type', $current_event->field_event_type->target_id)
-              ->condition('status', NodeInterface::PUBLISHED)
-              ->condition('nid', $current_event->id(), '<>')
-              ->sort('nid')
-              ->range(0, 3);
-    $nids  = $query->execute();
-    $nodes = Node::loadMultiple($nids);
-    
-    foreach ($nodes as $node) {
-      $options = ['absolute' => true, 'attributes' => ['class' => 'goto-event']];
-      $items[] = Link::fromTextAndUrl(
-        $node->getTitle(),
-        $node->toUrl('canonical', $options)
-      );
+        // si aucun evenement n'a été trouvé
+        // on affiche pas le bloc du tout
+        if (sizeof($nids) == 0) {
+            return false;
+        }
+        
+        $nodes = Node::loadMultiple($nids);
+        foreach ($nodes as $node) {
+            $options = ['absolute' => true, 'attributes' => ['class' => 'goto-event']];
+            $items[] = Link::fromTextAndUrl(
+                $node->getTitle(),
+                $node->toUrl('canonical', $options)
+            );
+        }
+
+        $build['content'] = [
+            '#theme'      => 'item_list',
+            '#list_type'  => 'ol',
+            '#items'      => $items,
+            '#attributes' => ['class' => 'similar-events'],
+        ];
+        return $build;
     }
-
-    $build['content'] = [
-      '#theme'      => 'item_list',
-      '#list_type'  => 'ol',
-      '#items'      => $items,
-      '#attributes' => ['class' => 'similar-events'],
-    ];
-    return $build;
-  }
-
 }
